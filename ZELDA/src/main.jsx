55863e5ed7b0f4631f7b970c0135c7dbaf55dc7d
11Won11 <hgg1hgf2@gmail.com>
2025-12-06 18:21:36 +0900

    ignore this

--- ZELDA/src/main.jsx
+++ ZELDA/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import StartPage from './components/startpage/StartPage.jsx' 
 import Level1 from './components/level1/Level1.jsx' 
+import Level2 from './components/level2/Level2.jsx' 
 import Level1Game from './components/level1/Level1Game.jsx' 
 import Level2Game from './components/level2/Level2Game.jsx'
 import AdminPage from './components/level1/AdminPage.jsx'
@@ -18,7 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/level1" element={<Level1 />} />
         <Route path="/level1Game" element={<Level1Game />} />
         <Route path="/admin-secret" element={<AdminPage />} />
-        <Route path="/level1" element={<Level2 />} />
+        <Route path="/level2" element={<Level2 />} />
         <Route path="/level2Game" element={<Level2Game />} />
         <Route path="/level3" element={<Level3 />} />
         <Route path="/level3Game" element={<Level3Game />} />
